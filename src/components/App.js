4a428567e478952bcf78a221f0e00b47205adc95
JCamiloAgu <jcagudelo42@misena.edu.co>
2019-12-11 12:10:30 -0500

    Se separaron las responsabilidades en la página de editar

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import BadgeNew from "../pages/BadgeNew";
 import Badges from "../pages/Badges";
 import NotFound from '../pages/NotFound'
 import BadgeEdit from '../pages/BadgeEdit'
+import BadgeDetails from '../pages/BadgeDetailsContainer'
 import Home from '../pages/Home';
 
 function App(){
@@ -17,6 +18,7 @@ function App(){
                     <Route exact path="/badges" component={Badges} />
                     <Route exact path="/badges/new" component={BadgeNew} />
                     <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
+                    <Route exact path="/badges/:badgeId" component={BadgeDetails} />
                     <Route component={NotFound}/>
                 </Switch>
             </Layout>
--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './styles/BadgesList.css';
-import Gravatar from './Gravatar';
+import "./styles/BadgesList.css";
+import Gravatar from "./Gravatar";
 
 class BadgesListItem extends React.Component {
   render() {
@@ -45,7 +45,10 @@ class BadgesList extends React.Component {
           {this.props.badges.map(badge => {
             return (
               <li key={badge.id}>
-                <Link className="text-reset text-decoration-none" to={`/badges/${badge.id}/edit`}>
+                <Link
+                  className="text-reset text-decoration-none"
+                  to={`/badges/${badge.id}/`}
+                >
                   <BadgesListItem badge={badge} />
                 </Link>
               </li>
