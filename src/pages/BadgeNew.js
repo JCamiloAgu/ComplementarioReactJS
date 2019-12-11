d181da31dcd1fce56f5c6c558ec04a5d5f5e44b3
JCamiloAgu <jcagudelo42@misena.edu.co>
2019-12-11 09:01:48 -0500

    Clase 18

---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,86 @@
+import React from "react";
+
+import "./styles/BadgeNew.css";
+import header from "../images/platziconf-logo.svg";
+import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
+import PageLoading from "../components/PageLoading";
+import api from "../api";
+
+class BagdeNew extends React.Component {
+  state = {
+    loading: false,
+    error: null,
+    form: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      jobTitle: "",
+      twitter: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    this.setState({ loading: true, error: null });
+
+    try {
+      await api.badges.create(this.state.form);
+      this.setState({ loading: false });
+      this.props.history.push('/badges')
+    } catch (error) {
+      this.setState({ loading: false, error: error });
+    }
+  };
+  render() {
+    if (this.state.loading) {
+      return <PageLoading />;
+    }
+    return (
+      <React.Fragment>
+        <div className="BadgeNew__hero">
+          <img
+            className="img-fluid BadgeNew__hero-image"
+            src={header}
+            alt="logo"
+          />
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <Badge
+                firstName={this.state.form.firstName || "FIRST NAME"}
+                lastName={this.state.form.lastName || "LAST NAME"}
+                twitter={this.state.form.twitter || "twitter"}
+                jobTitle={this.state.form.jobTitle || "JOB TITTLE"}
+                email={this.state.form.email || "EMAIL"}
+                avatarUrl="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50x"
+              />
+            </div>
+
+            <div className="col-6">
+            <h1>New Attendant</h1>
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+                onSubmit={this.handleSubmit}
+                error={this.state.error}
+              />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default BagdeNew;
